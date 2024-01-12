be6bac3219e91e17cade4b00acfcd2848888a803
Kenneth.Ajah <kenneth.ajah@stanbicibtc.com>
2024-01-12 14:21:54 +0100

    first commit

---
+++ src/TreeSet/RetrieveAndRemoveTheLowestAndHighestValue.java
@@ -0,0 +1,15 @@
+package TreeSet;
+
+import java.util.*;
+
+public class RetrieveAndRemoveTheLowestAndHighestValue {
+    public static void main(String args[]){
+        TreeSet<Integer> set=new TreeSet<Integer>();
+        set.add(24);
+        set.add(66);
+        set.add(12);
+        set.add(15);
+        System.out.println("Lowest Value: "+set.pollFirst());
+        System.out.println("Highest Value: "+set.pollLast());
+    }
+}
---
+++ src/TreeSet/TraversingIndescendingOrder.java
@@ -0,0 +1,19 @@
+package TreeSet;
+
+import java.util.*;
+
+public class TraversingIndescendingOrder {
+    public static void main(String args[]){
+        TreeSet<String> set=new TreeSet<String>();
+        set.add("Ravi");
+        set.add("Vijay");
+        set.add("Ajay");
+        System.out.println("Traversing element through Iterator in descending order");
+        Iterator i=set.descendingIterator();
+        while(i.hasNext())
+        {
+            System.out.println(i.next());
+        }
+
+    }
+}
